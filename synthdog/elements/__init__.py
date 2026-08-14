5a17c0ee630f65b29964f1d6c2f7090dd0bea8b1
Linh Phuong Nguyen <167509270+LinhPhuong14@users.noreply.github.com>
2026-08-14 06:06:30 +0000

    feat: synthdog vi receipt

---
+++ synthdog/elements/__init__.py
@@ -0,0 +1,14 @@
+"""
+Donut
+Copyright (c) 2022-present NAVER Corp.
+MIT License
+"""
+from elements.background import Background
+from elements.content import Content
+from elements.document import Document
+from elements.paper import Paper
+from elements.receipt import Receipt
+from elements.textbox import TextBox
+from elements.warp import CurlWarp
+
+__all__ = ["Background", "Content", "CurlWarp", "Document", "Paper", "Receipt", "TextBox"]
